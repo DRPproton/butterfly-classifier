bdd4f6caf375db3a709ae0576073ebf8b4a24b4d
Dashel Ruiz <dashel.ruiz@protonmail.com>
2023-12-19 16:21:35 -0600

    Adding convert to tflite file

---
+++ convert-model.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+from tensorflow import keras
+
+model = keras.models.load_model('resnet_50_v117_0.920.h5')
+
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+
+tflite_model = converter.convert()
+
+with open('butterfly-model.tflite', 'wb') as f_out:
+    f_out.write(tflite_model)
