84b55c34a49d15978844532dfdeb97f4285871b3
Dashel Ruiz <dashel.ruiz@protonmail.com>
2023-12-19 15:28:49 -0600

    Changes to the test for testing

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 import requests
 
-url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
+""" url for local testing """
+# url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
 
-# img = "https://i2.wp.com/www.dorsetbutterflies.com/wordpress/wp-content/uploads/2018/08/Adonis-Blue-James-Gould-2017-crop.jpg?fit=2103%2C1402&ssl=1"
+""" url to test the deployed lambda funcion """
+url = "https://19kywrz8ek.execute-api.us-east-1.amazonaws.com/butterfly-classifier/predict"
+
+img = "https://i2.wp.com/www.dorsetbutterflies.com/wordpress/wp-content/uploads/2018/08/Adonis-Blue-James-Gould-2017-crop.jpg?fit=2103%2C1402&ssl=1"
 
 
 data = {'url': "https://i2.wp.com/www.dorsetbutterflies.com/wordpress/wp-content/uploads/2018/08/Adonis-Blue-James-Gould-2017-crop.jpg?fit=2103%2C1402&ssl=1"}
