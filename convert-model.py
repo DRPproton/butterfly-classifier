290786d945ed18ef8f223f3804ca4700849d2efc
Dashel Ruiz <dashel.ruiz@protonmail.com>
2023-12-19 16:23:30 -0600

    adding coments

--- convert-model.py
+++ convert-model.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
 
+# Lodaing best model
 model = keras.models.load_model('resnet_50_v117_0.920.h5')
 
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
